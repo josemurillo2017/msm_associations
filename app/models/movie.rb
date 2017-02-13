e73450fa72ea4d1f7bb80c83d797c29b3e5adfc7
Jose Murillo <josemurillo2017@gmail.com>
2017-02-12 21:21:37 -0600

    Issues with links and show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,9 @@ class Movie < ApplicationRecord
   validates :director_id, :presence => true
   validates :title, :presence => true, :uniqueness => {:scope => :year}
   validates :year, :numericality => {:only_integer => true, :greater_than_or_equal_to => 1870, :less_than_or_equal_to => 2050}
-  validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800}
+  validates :duration, :numericality => {:only_integer => true, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 2764800,  :allow_blank => true}
 
-  belongs_to(:director, :class_name => "Director", :foreing_key => "director_id")
-  has_many(:characters, :class_name => "Character", :foreing_key => "movie_id")
+  belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+  has_many(:actors, :through => :characters)
 end
